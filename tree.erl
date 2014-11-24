559530497f7f4b9c72c0653e51313cd04a5ae308
Casey Leask <caseyleask@gmail.com>
2014-11-24 20:00:59 +1100

    tree module

--- tree.erl
+++ tree.erl
@@ -1,1 +1,23 @@
 -module(tree).
+
+-export([empty/0, insert/3, lookup/2]).
+
+empty() -> {node, 'nil'}.
+
+insert(Key, Value, {node, 'nil'}) ->
+  {node, {Key, Value, {node, 'nil'}, {node, 'nil'}}};
+insert(NewKey, NewValue, {node, {Key, Value, Smaller, Larger}}) when NewKey < Key ->
+  {node, {Key, Value, insert(NewKey, NewValue, Smaller), Larger}};
+insert(NewKey, NewValue, {node, {Key, Value, Smaller, Larger}}) when NewKey > Key ->
+  {node, {Key, Value, Smaller, insert(NewKey, NewValue, Larger)}};
+insert(Key, Val, {node, {Key, _, Smaller, Larger}}) ->
+  {node, {Key, Val, Smaller, Larger}}.
+
+lookup(_, {node, 'nil'}) ->
+  undefined;
+lookup(Key, {node, {Key, Value, _, _}}) ->
+  {ok, Value};
+lookup(IndexKey, {node, {Key, _, Smaller, _}}) when IndexKey < Key ->
+  lookup(IndexKey, Smaller);
+lookup(Key, {node, {_, _, _, Larger}}) ->
+  lookup(Key, Larger).
